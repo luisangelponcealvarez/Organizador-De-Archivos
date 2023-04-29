3063ba21c92deb570725086612e5d1ea9d459360
LuisAngelPonceAlvarez <poncealvarezluisangel4@gmail.com>
2023-04-28 23:02:56 -0600

    añado ejecutable de organizador de archivos

---
+++ OrganizadorDeArchivos.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import tkinter as tk
+from tkinter import filedialog
+import sys
+
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askdirectory()
+
+extensions = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp"],
+    "Videos": [".mp4", ".avi", ".mkv"],
+    "PDFs": [".pdf"],
+}
+
+for category, exts in extensions.items():
+    category_path = os.path.join(file_path, category)
+    os.makedirs(category_path, exist_ok=True)
+    for file in os.listdir(file_path):
+        if os.path.isfile(os.path.join(file_path, file)):
+            if os.path.splitext(file)[-1].lower() in exts:
+                shutil.move(
+                    os.path.join(file_path, file),
+                    os.path.join(category_path, file),
+                )
+
+tk.messagebox.showinfo(
+    "Organización completada", "Los archivos han sido organizados correctamente."
+)
+
+# Salir del proceso después de que se complete la organización
+sys.exit()
